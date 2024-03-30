e86e0ef41cd364601d9bdf8f768afead5a0d6ac7
Raj162002 <ramkumarrajeevan16@gmail.com>
2024-03-30 07:06:46 +0530

    Validation for form and border change has been done for first name last name and email

--- feedback_2.js
+++ feedback_2.js
@@ -1,21 +1,22 @@
 function previewview() {
-    var satisfactionLevel = document.querySelector('input[name="feedback_3"]:checked').value;
-    var recommendation = document.querySelector('input[name="feedback_4"]:checked').value;
-    var email = document.forms['feedback_form']['email'].value;
-    var firstName = document.forms['feedback_form']['first_name'].value;
-    var lastName= document.forms['feedback_form']['last_name'].value;
+    if(validateForm()){
+        let satisfactionLevel = document.querySelector('input[name="feedback_3"]:checked').value;
+        let recommendation = document.querySelector('input[name="feedback_4"]:checked').value;
+        let email = document.forms['feedback_form']['email'].value;
+        let firstName = document.forms['feedback_form']['first_name'].value;
+        let lastName= document.forms['feedback_form']['last_name'].value;
 
-    var preview =   'First Name: ' + firstName + '<br>' +
-                    'Last Name: ' + lastName + '<br>' +
-                    'Email: ' + email + '<br>' +
-                    'Satisfaction Level: ' + satisfactionLevel + '<br>' +
-                    'Would recommend: ' + recommendation;
+        let preview =   'First Name: ' + firstName + '<br>' +
+                        'Last Name: ' + lastName + '<br>' +
+                        'Email: ' + email + '<br>' +
+                        'Satisfaction Level: ' + satisfactionLevel + '<br>' +
+                        'Would recommend: ' + recommendation;
 
-    document.getElementById('previewSection').innerHTML = "<legend>Preview</legend>"+ preview;
-    // document.getElementById('previewSection').innerHTML = preview;
-    document.getElementById('previewSection').style.display = 'block';
-    document.getElementById('submitButton').style.display = 'block';
-    document.getElementById('previewButton').style.display = 'none';
+        document.getElementById('previewSection').innerHTML = "<legend>Preview</legend>"+ preview;
+        document.getElementById('previewSection').style.display = 'block';
+        document.getElementById('submitButton').style.display = 'block';
+        document.getElementById('previewButton').style.display = 'none';
+    }
 }
 
 document.getElementById('resetButton').addEventListener('click', function() {
@@ -26,40 +27,47 @@ document.getElementById('resetButton').addEventListener('click', function() {
 });
 
 function validateForm() {
-    var satisfactionLevel = document.querySelector('input[name="feedback_3"]:checked');
-    var recommendation = document.querySelector('input[name="feedback_4"]:checked');
-    var email = document.forms['feedback_form']['email'].value;
     var firstName = document.forms['feedback_form']['first_name'].value;
     var lastName = document.forms['feedback_form']['last_name'].value;
+    var email = document.forms['feedback_form']['email'].value;
+    var satisfactionLevel = document.querySelector('input[name="feedback_3"]:checked');
+    var recommendation = document.querySelector('input[name="feedback_4"]:checked');
 
-    if (!satisfactionLevel) {
-        alert('Please select a satisfaction level.');
-        document.getElementById('satisfaction').style.border = '2px solid red';
-        document.getElementById('satisfaction').scrollIntoView();
-        return false;
-    }
-    if (!recommendation) {
-        alert('Please select a recommendation.');
-        document.getElementById('recommendation').style.border = '2px solid red';
-        document.getElementById('recommendation').scrollIntoView();
-        return false;
-    }
+    document.getElementById('first_name_container').style.border = '';
+    document.getElementById('last_name_container').style.border = '';
+    document.getElementById('email_container').style.border = '';
+
     if(firstName == "") {
         alert('Please enter your first name.');
-        document.getElementById('first_name').style.border = '2px solid red';
-        document.getElementById('first_name').scrollIntoView();
+        document.getElementById('first_name_container').style.border = '4px solid red';
+        document.getElementById('first_name_container').scrollIntoView();
         return false;
     }
     if(lastName == "") {
         alert('Please enter your last name.');
-        document.getElementById('last_name').style.border = '2px solid red';
-        document.getElementById('last_name').scrollIntoView();
+        document.getElementById('last_name_container').style.border = '4px solid red';
+        document.getElementById('last_name_container').scrollIntoView();
         return false;
     }
 
     var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if (!emailRegex.test(email)) {
         alert('Please enter a valid email address.');
+        document.getElementById('email_container').style.border = '4px solid red';
+        document.getElementById('email_container').scrollIntoView();
+        return false;
+    }
+
+    if (satisfactionLevel==null) {
+        alert('Please select a satisfaction level.');
+        document.getElementById('satisfaction').style.border = '4px solid red';
+        document.getElementById('satisfaction').scrollIntoView();
+        return false;
+    }
+    if (recommendation==null) {
+        alert('Please select a recommendation.');
+        document.getElementById('recommendation').style.border = '4px solid red';
+        document.getElementById('recommendation').scrollIntoView();
         return false;
     }
     return true;
