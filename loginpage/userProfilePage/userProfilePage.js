8e505ddcd2f64d1cc24f7d267a0f07e3d2bcff27
Thevinu Gunawardena <theviguna@gmail.com>
2024-04-02 10:53:38 +0530

    remove progress bar

--- loginpage/userProfilePage/userProfilePage.js
+++ loginpage/userProfilePage/userProfilePage.js
@@ -8,45 +8,115 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // Define your question bank
     const questionBank = [{
-        heading: "Step 1 Personal Details | Question 1/5",
+        heading: "Step 1 Personal Details | Question 1/6",
         number: "1",
         question: "Enter your name: ",
+        type: "text"
 
     },
     {
-        heading: "Step 1 Personal Details | Question 2/5",
+        heading: "Step 1 Personal Details | Question 2/6",
         number: "2",
-        question: "Enter surname: "
+        question: "Enter surname: ",
+        type: "text"
 
         
     },
     {
-        heading: "Step 1 Personal Details | Question 3/5",
+        heading: "Step 1 Personal Details | Question 3/6",
         number: "3",
-        question: "Enter username: "
+        question: "Enter username: ",
+        type: "text"
     },
     {
-        heading: "Step 1 Personal Details | Question 4/5",
-        number: "4",
-        question: "Enter password: "
+        heading: "Step 1 Personal Details | Question 4/6",
+        number: "3",
+        question: "Enter email: ",
+        type: "email"
+
 
     },
     {
-        heading: "Step 1 Personal Details | Question 5/5",
-        number: "4",
-        question: "Enter password: "
+        heading: "Step 1 Personal Details | Question 5/6",
+        number: "5",
+        question: "Enter password: ",
+        type: "password"
+
     },
+    {
+        heading: "Step 1 Personal Details | Question 6/6",
+        number: "6",
+        question: "Enter password: ",
+        type: "password"
+    },
+    {
+        heading: "Step 2 Educational qualifications | Question 1/5 ",
+        number: "7",
+        question: "Areas of study: ",
+        type: "text"
+
+    },
+    {
+        heading: "Step 2 Educational qualifications | Question 2/5",
+        number: "8",
+        question: "Highest degree: ",
+        type: "text" 
+    },
+    {
+        heading:"Step 2 Educational qualifications | Question 3/5",
+        number: "9",
+        question: "University/Insitution: ",
+        type: "text"
+    },
+    {
+        heading: "Step 2 Educational qualifications | Question 4/5",
+        number: "10",
+        question: "Completion year: ",
+        type: "text"
+    },
+    {
+        heading: "Step 2 Educational qualifications | Question 5/5",
+        number: "11",
+        question: "Country: ",
+        type: "text"
+    },
+    {
+        heading: "Step 3 Contact details | Question 1/4",
+        number: "12",
+        question: "Employee status (if applicable): ",
+        type: "text"
+    },
+    {
+        heading: "Step 3 Contact details | Question 2/4",
+        number: "12",
+        question: "Work place address (if applicable): ",
+        type: "text"
+    },
+    {
+        heading: "Step 3 Contact details | Question 3/4",
+        number: "13",
+        question: "Telephone number: ",
+        type: "tel"
+    },
+    {
+        heading: "Step 3 Contact details | Question 4/4",
+        number: "14",
+        question: "Home address: ",
+        type: "text"
+    }
+
     
-
     ];
     
     let currentQuestionIndex = 0;
     
     // Function to set the question
     function setQuestion(index) {
+        const currentQuestion = questionBank[index];
         heading.textContent = questionBank[index].heading;
         questionLabel.textContent = questionBank[index].question;
-        questionAnswerInput.value = ''; // Clear input field
+        questionAnswerInput.value = ''; 
+        questionAnswerInput.type = currentQuestion.type;
     }
     
     // Function to handle submitting the answer
@@ -69,8 +139,7 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // Function to handle skipping the question
     function skipQuestion() {
-        // You can set a default value here if needed
-        submitAnswer(); // Submit the default value as an answer
+        submitAnswer(); 
     }
     
     // Event listener for the submit button
@@ -79,13 +148,19 @@ document.addEventListener("DOMContentLoaded", function() {
     // Event listener for the skip button
     skipButton.addEventListener('click', skipQuestion);
     
-    // Function to update the progress bar
+    // update progress bar
     function updateProgress() {
         const progressText = document.querySelector('.progress-text');
         const progressFill = document.querySelector('.progress-fill');
+        const progress = document.querySelector('.progress');
         const percentage = ((currentQuestionIndex + 1) / questionBank.length) * 100;
         progressText.textContent = `${percentage.toFixed(0)}%`;
         progressFill.style.width = `${percentage.toFixed(0)}%`;
+        if (progressFill.style.width == "100%"){
+            console.log("test")
+            progress.style.display = "none";
+
+        }
     }
     
     // Initialize the first question
